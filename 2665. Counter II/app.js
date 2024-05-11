593a8da3eeecf13523916f776c3bf98155f8c107
Пак Александр Климентьевич <akpak@beeline.kz>
2024-05-11 23:12:27 +0500

    resolve 2665

---
+++ 2665. Counter II/app.js
@@ -0,0 +1,17 @@
+const createCounter = function (init) {
+    let num = init
+    return {
+        increment: function () {
+            num = num + 1
+            return num
+        },
+        decrement: function () {
+            num = num - 1
+            return num
+        },
+        reset: function () {
+            num = init
+            return num
+        }
+    }
+};
